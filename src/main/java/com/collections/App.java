5f366e4cde0ff2adf4b6f9b89f05a132047bd221
Jakub Rogowski <jakerogowski33@gmail.com>
2022-07-03 12:35:22 +0200

    set.add()

--- src/main/java/com/collections/App.java
+++ src/main/java/com/collections/App.java
@@ -1,9 +1,22 @@
 package com.collections;
 
-public class App 
+import java.util.HashSet;
+import java.util.Set;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Set<String> setOfStrings = new HashSet<>();
+        setOfStrings.add("Alicja");
+        setOfStrings.add("Jakub");
+        setOfStrings.add("Julia");
+        setOfStrings.add("Aleksandra");
+        setOfStrings.add("Marcin");
+        setOfStrings.add("Bartosz");
+        setOfStrings.add("Bartosz");
+
+        System.out.println(setOfStrings.size());
+
     }
 }
