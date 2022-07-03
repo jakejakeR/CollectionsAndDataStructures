af58b12e7823c1baffccacb86fe3744801e0eefb
Jakub Rogowski <jakerogowski33@gmail.com>
2022-07-03 21:51:39 +0200

    Task 8b

--- src/main/java/com/collections/App.java
+++ src/main/java/com/collections/App.java
@@ -3,17 +3,38 @@ package com.collections;
 import com.collections.factories.CarFactory;
 import com.collections.models.Car;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        List<Car> randomCars = new CarFactory().createRandomCars(3);
+        CarFactory carFactory = new CarFactory();
+        List<Car> randomCars = carFactory.createRandomCars(5);
+
         randomCars.forEach(System.out::println);
         System.out.println("===========");
 
-        randomCars.sort(Comparator.comparing(car -> car.getEngine().getCapacity()));
-        randomCars.forEach(System.out::println);
+        Comparator<Car> compareDescendingCapacity = (car1, car2) -> car2.getEngine().getCapacity() - car1.getEngine().getCapacity();
+        Comparator<Car> comparePower = Comparator.comparingInt(car -> car.getEngine().getPower());
+        Comparator<Car> compareColor = Comparator.comparingInt(car -> car.getColor().ordinal());
+
+        List<Car> sortByCapacityDescending = sortCars(randomCars, compareDescendingCapacity);
+        sortByCapacityDescending.forEach(System.out::println);
+        System.out.println("===========");
+
+        List<Car> sortByPower= sortCars(randomCars, comparePower);
+        sortByPower.forEach(System.out::println);
+        System.out.println("===========");
+
+        List<Car> sortByColor = sortCars(randomCars, compareColor);
+        sortByColor.forEach(System.out::println);
+    }
+
+    public static List<Car> sortCars(List<Car> cars, Comparator<Car> comparator) {
+        Collections.sort(cars, comparator);
+        List<Car> carList = cars;
+        return carList;
     }
 }
 
