33cc0d86b5d7cd4bbc825778074556dc8c1c6513
Jakub Rogowski <jakerogowski33@gmail.com>
2022-07-03 20:43:46 +0200

    Iterator replaced with Collection.removeIf

--- src/main/java/com/collections/App.java
+++ src/main/java/com/collections/App.java
@@ -2,20 +2,13 @@ package com.collections;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-        Iterator<Integer> iterator = numbers.iterator();
 
-        while (iterator.hasNext()) {
-            Integer next = iterator.next();
-            if (next % 2 == 0) {
-                iterator.remove();
-            }
-        }
+        numbers.removeIf(number -> number % 2 == 0);
         System.out.println(numbers);
     }
 }
